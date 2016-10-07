0da7f09c29a44cb70ddc6217e934f0c299bf1fd0
fabianha <fabian.hafner@gmail.com>
2016-10-07 15:27:50 +0200

    PushingMyPackage

---
+++ Week5/class_packs/FabiansPackage/R/HighestNumber.R
@@ -0,0 +1,13 @@
+#' Finding the highest number in a vector
+#'
+#' This function finds the biggest number in a vector
+#' @export
+#' @examples
+#' vector_max()
+
+
+### Find the highest number in a vector
+vector_max <- function(vector){
+  vector <- c(vector)
+  return(max(vector))
+}
